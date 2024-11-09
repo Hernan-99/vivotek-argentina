62addd6b7dd0b757d6f3f9a2aa0aa01bbac44916
Hernan <hernan24744@gmail.com>
2024-11-08 21:04:53 -0300

    actualizacion del index

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <Carousel />
 
     <section class="container mb-8">
-      <h3 class="text-2xl md:text-3xl lg:text-4xl font-bold mb-8 lg:mb-20">
+      <h3
+        class="text-center lg:text-start text-2xl md:text-3xl lg:text-4xl font-bold mb-8 lg:mb-20"
+      >
         Soporte y aprendizaje
       </h3>
 
@@ -36,16 +38,87 @@
         </div>
         <!-- Card 2 -->
         <div class="card-soporte">
-          <div class="card-header"></div>
-          <div class="card-body"></div>
+          <div class="card-header">
+            <NuxtLink>
+              <img
+                class="rounded-t-lg"
+                src="~/assets/images/card-img-2.webp"
+                alt="Ver mas inteligente imagen"
+              />
+            </NuxtLink>
+          </div>
+          <div class="card-body p-5">
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              Tecnología Avanzada
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              VIVOTEK ofrece tecnología de avanzada, obtenga más información
+              sobre la tecnología aplicada en Vivotek.
+            </p>
+          </div>
         </div>
         <!-- Card 3 -->
         <div class="card-soporte">
-          <div class="card-header"></div>
-          <div class="card-body"></div>
+          <div class="card-header">
+            <NuxtLink>
+              <img
+                class="rounded-t-lg"
+                src="~/assets/images/card-img-3.webp"
+                alt="Ver mas inteligente imagen"
+              />
+            </NuxtLink>
+          </div>
+          <div class="card-body p-5">
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              Soporte técnico
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              VIVOTEK brinda soluciones en Argentina a través de C-Team, un
+              lugar donde podés hacer tus preguntas acerca de los productos
+              Vivotek
+            </p>
+          </div>
         </div>
       </article>
     </section>
+
+    <div class="w-full h-[300px] lg:h-[700px] relative">
+      <video
+        class="w-full h-full object-cover"
+        src="~/assets/media/vivotek.mp4"
+        autoplay
+        loop
+      ></video>
+      <div
+        class="w-full h-full top-0 overlay absolute bg-blue-700 opacity-40"
+      ></div>
+      <div
+        class="absolute top-0 w-full h-full flex justify-center items-center text-white"
+      >
+        <div class="container-descripcion">
+          <h1 class="mb-2 text-xl md:text-3xl font-bold md:mb-8">
+            Somos VIVOTEK Argentina
+          </h1>
+          <p class="mb-4 text-sm md:text-xl md:mb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            enim nesciunt numquam quam quos exercitationem nisi explicabo totam
+            harum blanditiis officia voluptas dolore, quibusdam, architecto fuga
+            consectetur amet, ex praesentium.
+          </p>
+          <button
+            class="mb-2 bg-blue-500 font-bold text-white px-8 py-2 xl:px-20 xl:py-4 rounded-md hover:bg-blue-600 hover:shadow-blue-500 hover:shadow-xl duration-300"
+          >
+            Contactanos
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <section></section>
   </div>
 </template>
 
