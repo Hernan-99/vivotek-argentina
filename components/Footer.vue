416184ef392372caf3b3393a4f59497079b65b37
Hernan <hernan24744@gmail.com>
2024-11-08 21:19:29 -0300

    Estructura del footer mas newsletter

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,30 @@
 <template>
-  <footer>
-    <h1>Footer</h1>
+  <footer class="bg-blue-300">
+    <!-- Newsletter -->
+    <section class="container text-white top-0 right-0 left-0 -translate-y-1/2">
+      <div class="bg-blue-700 px-10 pt-5 pb-10 space-y-5 rounded-md">
+        <h3><span>Suscribite</span> a nuestro newsletter</h3>
+        <div class="flex flex-col md:flex-row gap-1">
+          <input
+            class="w-full px-5 py-3 text-blue-900 rounded-md outline-none"
+            type="email"
+            placeholder="ejemplo@email.com"
+          />
+          <button
+            class="flex items-center gap-1 bg-gray-950 px-5 py-3 rounded-md hover:opacity-80 duration-300 justify-center"
+          >
+            <span>Suscribirse</span>
+          </button>
+        </div>
+      </div>
+    </section>
+    <section class="container"></section>
+    <section class="container"></section>
+    <section class="bg-blue-950">
+      <p class="text-center mt-10 text-white p-5">
+        Copyright &copy; All rights reserved
+      </p>
+    </section>
   </footer>
 </template>
 
