12a051bc6121729fb339a865fdb25c0d6ef5f198
Hernan <hernan24744@gmail.com>
2024-11-17 15:12:04 -0300

    Logica para consumir json y renderizar las tarjetas

--- components/CardProductos.vue
+++ components/CardProductos.vue
@@ -15,24 +15,26 @@
       class="mb-20 container w-full grid grid-cols-1 gap-x-8 gap-y-36 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
     >
       <div
-        class="productos__card bg-blue-500 p-10 pt-24 rounded-md relative"
-        v-for="(data, index) in data_item"
+        class="bg-blue-500 productos__card rounded-md"
+        v-for="(item, index) in items"
         :key="index"
       >
-        <img
-          class="w-56 absolute -top-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 duration-500"
-          :src="data.image"
-          alt=""
-        />
-        <p class="italic text-blue-900">vivotek</p>
-        <h3
-          class="mb-6 text-white text-center lg:text-start text-md md:text-xl font-bold"
-        >
-          {{ data.titulo }}
-        </h3>
-        <div class="mb-0">
+        <div class="productos__card p-10 pt-16 py-6 relative">
+          <img
+            class="w-56 absolute -top-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 duration-500"
+            :src="item.imagen_referencia"
+            alt=""
+          />
+          <p class="italic text-blue-900">vivotek</p>
+          <h3
+            class="mb-4 text-white text-center lg:text-start text-md md:text-xl font-bold"
+          >
+            {{ item.tipo }}
+          </h3>
+        </div>
+        <div class="mb-0 px-3 p-8 text-center w-full">
           <NuxtLink
-            class="w-full text-white bg-blue-900 px-2 py-1 rounded-md"
+            class="w-full text-white bg-blue-900 px-6 py-3 rounded-md"
             to="/gran_angular_products"
           >
             Mostrar modelos
@@ -51,27 +53,81 @@
 </template>
 
 <script setup>
-import camarasdomo from "~/assets/images/camarasdomo.png";
-import domosfijos from "~/assets/images/domosfijos.png";
-import granangular from "~/assets/images/granangular.png";
-import domosptz from "~/assets/images/domosptz.png";
+// muestra los primeros 4
+// import { ref } from "vue";
+// const items = ref({});
+// const url = "https://json-data-vivotek.vercel.app/data.json";
+// fetch(url)
+//   .then((res) => {
+//     if (!res.ok) {
+//       throw new Error("Error");
+//     }
+//     return res.json();
+//   })
+//   .then((data) => {
+//     //convierto el objeto a un array
+//     items.value = Object.values(data.camaras).slice(0, 4);
+//   })
+//   .catch((err) => {
+//     console.log("error ", err);
+//   });
 
-const data_item = [
-  {
-    titulo: "Cámaras Bullet",
-    image: camarasdomo,
-  },
-  {
-    titulo: "Domos Fijos",
-    image: domosfijos,
-  },
-  {
-    titulo: "Cámaras Gran Angular",
-    image: granangular,
-  },
-  {
-    titulo: "Domos PTZ",
-    image: domosptz,
-  },
-];
+import { ref, onMounted } from "vue";
+
+const items = ref([]);
+const url = "https://json-data-vivotek.vercel.app/data.json";
+
+// fetch(url)
+//   .then((res) => {
+//     if (!res.ok) {
+//       throw new Error("Error");
+//     }
+//     return res.json();
+//   })
+//   .then((data) => {
+//     // Filtrar solo los tipos de productos que quieres mostrar
+//     const camaras_card = [
+//       "Cámaras Gran Angular",
+//       "Cámaras Termicas",
+//       "Cámaras Domo",
+//       "NVR",
+//     ];
+
+//     // Filtrar los productos según los tipos deseados
+//     items.value = Object.values(data.camaras).filter((item) =>
+//       camaras_card.includes(item.tipo)
+//     );
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+const getData = async () => {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error("Error al obtener los datos");
+    }
+    const data = await res.json();
+    const camaras_card = [
+      "Cámaras Gran Angular",
+      "Cámaras Termicas",
+      "Cámaras Domo",
+      "NVR",
+    ];
+
+    //convertimos el objeto a un array y despues usamos el metodo filter para filtrar lo que vamos a mostrar
+    items.value = Object.values(data.camaras).filter((item) => {
+      camaras_card.includes(item.tipo);
+    });
+
+    console.log(
+      (items.value = Object.values(data.camaras).filter((item) => {
+        return camaras_card.includes(item.tipo);
+      }))
+    );
+  } catch (error) {}
+};
+
+onMounted(getData);
 </script>
